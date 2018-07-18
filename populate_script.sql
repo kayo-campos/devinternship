63d4b350e553a9ec55e5a02a66075fa5d6cb9b25
PuckmanXY <kayoanderson0403@gmail.com>
2018-07-18 11:12:41 -0300

    1. dba is DONE

---
+++ populate_script.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS tbl_tipos_pessoas;
+DROP TABLE IF EXISTS tbl_pessoas;
+
+CREATE TABLE IF NOT EXISTS tbl_tipos_pessoas (
+	id SERIAL PRIMARY KEY,
+	descricao VARCHAR(8)
+);
+
+CREATE TABLE IF NOT EXISTS tbl_pessoas (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(30) NOT NULL,
+	email VARCHAR(30) NOT NULL,
+	telefone VARCHAR(15),
+	tipo_id INT references tbl_tipos_pessoas(id),
+	CONSTRAINT validate_telefone CHECK (	
+		telefone SIMILAR TO '\(([0-9]{2})\)([0-9]{4})\.([0-9]{4})'
+		OR
+		telefone SIMILAR TO '\(([0-9]{2})\)([0-9]{3})\.([0-9]{3})\.([0-9]{3})'
+	),
+	CONSTRAINT validate_email CHECK (
+		email SIMILAR TO '[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}'
+	)
+);
+
+INSERT INTO tbl_tipos_pessoas (descricao)
+VALUES ('Física'), ('Jurídica');
+
+INSERT INTO tbl_pessoas (nome, email, telefone, tipo_id)
+VALUES	
+-- ACCEPTED VALUES
+		('João', 'email1@email.com', '(85)9090.8080', 1),
+		('Maria', 'email2@email.com', '(85)101.101.101', 1);
+-- REFUSED VALUES
+--		('Kleber', 'email3@email.com', '(800)8080.10000', 2),
+--		('Jhonatan', 'not-an_email.com', '(85)8080.8080', 1),
+--		('Jackson', 'invalid@email.coooooom', '(88)808.808.808', 2)
